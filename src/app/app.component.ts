a46d919a31660de915a75454f33023f1fc7a0af8
Boris Joskic <boris.joskic@jsguru.io>
2018-12-28 16:05:22 +0100

    Init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,117 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component, EventEmitter,
+  forwardRef,
+  Input,
+  OnChanges,
+  OnInit, Output,
+  QueryList,
+  SimpleChanges,
+  ViewChild,
+  ViewChildren
+} from '@angular/core';
+import {SortableComponent} from 'ng2-dnd';
+import {ControlValueAccessor} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit, OnChanges, ControlValueAccessor, AfterViewInit {
+  panelOpenState: boolean = false;
+
+  public panel = [
+    {
+      title: 'Panel1',
+      content: 'Panel content',
+      expanded: false
+    },
+    {
+      title: 'Panel2',
+      content: 'Panel content',
+      expanded: false
+    },
+    {
+      title: 'Pane3l',
+      content: 'Panel content',
+      expanded: false
+    },
+    {
+      title: 'Panel4',
+      content: 'Panel content',
+      expanded: false
+    },
+    {
+      title: 'Panel5',
+      content: 'Panel content',
+      expanded: false
+    },
+    {
+      title: 'Panel6',
+      content: 'Panel content',
+      expanded: false
+    },
+    {
+      title: 'Panel7',
+      content: 'Panel content',
+      expanded: false
+    },
+    {
+      title: 'Panel8',
+      content: 'Panel content',
+      expanded: false
+    },
+    {
+      title: 'Panel9',
+      content: 'Panel content',
+      expanded: false
+    }
+  ];
+
+  @ViewChildren(SortableComponent) draggables: QueryList<SortableComponent>;
+  private clone: HTMLElement;
+
+
+  ngAfterViewInit() {
+    // this.draggables.changes.subscribe(() => {
+    //   this.draggables.forEach((ref) => {
+    //     ref._elem.addEventListener('dragstart', (event) => {
+    //       this.clone = <HTMLElement>event.srcElement.children[0].cloneNode(true);
+    //       this.clone.id = 'clone';
+    //       this.clone.classList.remove('dnd-sortable-drag');
+    //       this.clone.classList.add('clone');
+    //       document.body.appendChild(this.clone);
+    //       event.dataTransfer.setDragImage(this.clone, 0, 0);
+    //     }, false);
+    //     ref._elem.addEventListener('dragend', (event) => {
+    //       let t = document.getElementById('clone');
+    //       if (t) {
+    //         t.remove();
+    //       }
+    //     }, false);
+    //   });
+    // });
+  }
+
+  public addAccordion() {
+    this.panel.push({
+      title: 'Panel added',
+      content: 'Panel content',
+      expanded: false
+    });
+  }
+
+  public openAll() {
+    this.panel.forEach(i => {
+      i.expanded = true
+    })
+  }
+
+  public closeAll() {
+    this.panel.forEach(i => {
+      i.expanded = false
+    })
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
+import {DndModule} from 'ng2-dnd';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatIconModule} from '@angular/material/icon';
+import {MatSelectModule} from '@angular/material/select';
+import {MatCheckboxModule} from '@angular/material/checkbox';
 
 
 import { AppComponent } from './app.component';
+import {MatInputModule} from '@angular/material';
+import {MatCardModule} from '@angular/material/card';
 
 
 @NgModule({
@@ -10,7 +21,18 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatButtonModule,
+    MatExpansionModule,
+    MatFormFieldModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatSelectModule,
+    MatInputModule,
+    MatCardModule,
+    MatCheckboxModule,
+    DndModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
